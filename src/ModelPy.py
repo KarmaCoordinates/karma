f3e6d1d1eedfb39e006e3443ae8ba1110c512347
Sandeep Dixit (BMM) <sdixitbmm@Sandeeps-MacBook-Pro-2023.local>
2024-07-08 14:47:04 -0400

    content and resource updates

--- src/ModelPy.py
+++ src/ModelPy.py
@@ -21,38 +21,47 @@ show_eda = False
 st.title('Karma Coordinates Prediction App')
 
 # web content
-st.image('karma/kapil-muni-image.png', caption='Kapil muni 5000 BC')
+st.image('karma/resources/kapil-muni-image.png', caption='Kapil muni 5000 BC')
 
-txt = '''According to Sankhya-yoga Prakriti (the universe) exists for providing
-experiences  to Purush. Purush upon realization that “I exist” is liberated
-(Moksha).  Every life form in Prakriti is engaged in providing experiences to a
+pdf = '<a href="{karma/resources/samkhya-karika.pdf}">pdf</a>'
+
+txt = f'''According to Sankhya-yoga, Prakriti (the universe) exists for providing
+experiences to Purush. Purush upon realization that “I exist” is liberated
+(Moksha). Every life-form in Prakriti is engaged in providing experiences to a
 Purush. Once  all Purush are awakened, the Prakriti’s work is complete and it
-collapses into  a singularity and the next cycle is started with a big-bang. As
+collapses into a singularity and the next cycle is started with a big-bang. As
 per the open model with omega of 6, universe is 6 billion years old and it
-will end when it is about 13 billion years old. That is in another 7
+will end when it is about 13 billion years old - that is in another 7
 billion years. (https://en.wikipedia.org/wiki/Ultimate_fate_of_the_universe) 
-A life form comes into an existence due to a microscopic particle in
-nature called as Sukshm. Human life is the only known form of Sukshm that is
-capable of achieving Moksha for its Purush.   Sankhya-yoga quantifies and
-explains attributes (Gunas) and tendencies (Bhavas) that can take a life form
-closer to achieving Moksha.   Karma Coordinates is a fun app developed to
-approximate based on your Gunas and Bhavas your current position in this karmic
-journey of many lives until Moksha.    A score of 5 billion years, means you
-are already at about 11 billion year mark - very near to achieving the
-awakening state. A score of 1 billion years means you are at 7 billion year
-mark, a little bit ahead but can speed it up by certain lifestyle based
-changes.  Karma Coordinates outcome is also explained in terms of three Gunas -
-Satva, Rajas and Tamas:  Satva is the light (Prakash) property in the
-Prakriti. The neural network in our brain - our intellect - has the highest
-Satva. Rajas is the energy property in the Prakriti. It moves mass. It
-activates. Our mind and bodies are enabled by Rajas. Tamas is the mass property
-in the Prakriti - The flesh and bones of our body has highest Tamas.  
+
+A life-form comes into an existence due to a microscopic particle in
+nature called as Sukshm. It is Sukshm that gets tinged with the acquired 
+tendencies (Bhavas), from life-form to life-form! Once all tendencies are 
+overcome/consumed, Moksha is achieved. A human life is the only known form 
+of Sukshm that is capable of achieving Moksha for its Purush. Sankhya-yoga 
+quantifies and explains attributes (Gunas) and tendencies (Bhavas) that can 
+take a life form closer to achieving Moksha. 
+
+Karma Coordinates is a fun app developed to approximate based on your Gunas and 
+Bhavas your current position in this karmic journey of many lives until Moksha. 
+A score of 11/13 means you are at about 11 billion year mark - very near 
+to achieving the awakening state.  A score of 7/13 means you are at 7 billion 
+year mark, ahead but can speed it up by certain lifestyle based changes. 
+
+Karma Coordinates outcome is also explained in terms of three Gunas - Satva, 
+Rajas and Tamas:  
+- Satva is the light (Prakash) property in the Prakriti. The neural network 
+in our brain - our intellect - has the highest Satva. 
+- Rajas is the energy property in the Prakriti. It moves mass. It activates. 
+Our mind and bodies are enabled by Rajas. 
+- Tamas is the mass property in the Prakriti - The flesh and bones of our body 
+has the highest Tamas.  
 '''
 
 st.text(txt)
 
 # Load the data
-df = pd.read_csv('kc3_synthout_chunk_0.csv')
+df = pd.read_csv('karma/resources/kc3_synthout_chunk_0.csv')
 df = df.drop(columns=['scaled_level', 'karma_coordinates'])
 
 # Overall Statistics
@@ -132,7 +141,7 @@ if show_eda:
     st.write('The confusion matrix shows the number of correct and incorrect predictions made by the model. Each row represents the actual class, while each column represents the predicted class. The diagonal values indicate correct predictions.')
 
 # Create input fields for user
-st.subheader('Input Features')
+st.subheader('Calculate my Karma Coordinates')
 user_input = {}
 for col in X.columns:
     if col in categorical_cols:
@@ -148,9 +157,21 @@ prediction = model.predict(input_df)
 prediction_label = label_encoder.inverse_transform(prediction)
 
 # Display prediction
-st.subheader('Prediction')
-st.write(f'The predicted karma coordinates label is: {prediction_label[0]}')
-st.write('The prediction above indicates the most likely karma coordinates label based on the input features provided.')
+st.subheader('AI prediction')
+st.write(f'# Your Karma Coordinates: **{prediction_label[0]}**')
+#st.write('The prediction above indicates the most likely karma coordinates label based on the input features provided.')
+
+# User feedback
+st.subheader('Feedback')
+satva_choice = st.selectbox('Satva', ('Dominant', 'High', 'Moderate', 'Low'))
+tamas_choice = st.selectbox('Tamas', ('Low', 'Moderate', 'High', 'Dominant'))
+
+st.subheader('Improving Karma Coordinates')
+
+st.subheader('Including zodiac sign and horoscope in calculations')
+
+st.subheader('Recommended life-style improvement resources')
+
 
 # Option to download the result as PDF
 def create_pdf(input_data, prediction):
