5f5207dd167ea0689754cbd9e1424cfb3986cdfa
Sandeep Dixit <sdixit@ohioedge.com>
2024-07-19 14:01:34 -0400

    added logger

--- src/file_functions.py
+++ src/file_functions.py
@@ -2,11 +2,14 @@ import pickle
 import boto3
 import pandas as pd
 from pathlib import Path
+import logging
 
 temp_folder = 'resources'
+logging.basicConfig(filename=f'{temp_folder}/streamlit-app.log', filemode='w', level=logging.INFO)
 
 # return df
 def load_csv(bucket_name, object_key):
+    logging.info('calling load_csv')
     s3_uri = f's3://{bucket_name}/{object_key}'
     # s3file = ff.read_from_s3('karmacoordinates', 'kc3_synthout_chunk_0.csv')
     # df = pd.read_csv(f'{resources_folder}/kc3_synthout_chunk_0.csv')
@@ -14,6 +17,7 @@ def load_csv(bucket_name, object_key):
 
 
 def save_obj(obj, bucket_name, object_key):
+    logging.info('calling save_obj')
     # save the model to disk
     local_uri = f'{temp_folder}/{object_key}'
     pickle.dump(obj, open(local_uri, 'wb'))
@@ -23,16 +27,17 @@ def save_obj(obj, bucket_name, object_key):
     
 
 def load_obj(bucket_name, object_key):
+    logging.info('calling load_obj')
     local_uri = f'{temp_folder}/{object_key}'
     objfile = Path(local_uri)
     if objfile.is_file():
-        print(f'using local file:{local_uri}')
+        logging.info(f'using local file:{local_uri}')
         return pickle.load(open(local_uri, 'rb'))
     else:
         s3_uri = f's3://{bucket_name}/{object_key}'
-        print(f'reading s3 file:{s3_uri}')
+        logging.info(f'reading s3 file:{s3_uri}')
         data = pd.read_pickle(s3_uri)
-        print(f'saving s3 file:{s3_uri} locally at: {local_uri}')
+        logging.info(f'saving s3 file:{s3_uri} locally at: {local_uri}')
         pickle.dump(data, open(local_uri, 'wb'))
         return data
     # s3_uri = f'/tmp/{object_key}'
