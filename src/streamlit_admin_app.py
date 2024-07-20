127b608cd800e00d301808650843a56df1f60f58
Sandeep Dixit <sdixit@ohioedge.com>
2024-07-19 22:23:36 -0400

    perf improvements

--- src/streamlit_admin_app.py
+++ src/streamlit_admin_app.py
@@ -4,12 +4,16 @@ import functions
 import web_content 
 
 @st.cache_data
-def cache_load_csv(bucket_name, features_data_file):
-    return ff.load_csv(bucket_name, features_data_file)
+def cache_model(model_choice, bucket_name, features_data_file, pickled_model_data_file):
+    df = ff.load_csv(bucket_name, features_data_file)
+    df, X, y, label_encoder = functions.read_features(df)
+    categorical_cols, numeric_cols, preprocessor = functions.encode_features(X)
+    model, X_train, X_test, y_train, y_test = functions.define_model(X, y, model_choice, preprocessor)
+    model = ff.load_obj(bucket_name, pickled_model_data_file)
 
-@st.cache_data
-def cache_load_obj(bucket_name, pickled_model_data_file):
-    return ff.load_obj(bucket_name, pickled_model_data_file)
+    accuracy, conf_matrix = functions.model_eval(model, X_test, y_test)
+
+    return df, X.columns, categorical_cols, model, label_encoder, accuracy, conf_matrix
 
 def run_app():
 
@@ -21,24 +25,16 @@ def run_app():
     web_content.write_content(resources_folder)
 
     model_choice = 'RandomForest'
-    df = cache_load_csv(bucket_name, features_data_file)
-    df, X, y, label_encoder = functions.read_features(df)
-    categorical_cols, numeric_cols, preprocessor = functions.encode_features(X)
-
-    model, X_train, X_test, y_train, y_test = functions.define_model(X, y, model_choice, preprocessor)
-
-    model = cache_load_obj(bucket_name, pickled_model_data_file)
+    df, columns, categorical_cols, model, label_encoder, accuracy, conf_matrix = cache_model(model_choice, bucket_name, features_data_file, pickled_model_data_file)
 
 
     functions.show_stats(df)
-    functions.show_eda(df, X, categorical_cols)
+    functions.show_eda(df, columns, categorical_cols)
     functions.show_models()
-
-    accuracy, conf_matrix = functions.model_eval(model, X_test, y_test)
 
     functions.model_perf(accuracy, conf_matrix)
 
-    input_df, user_input = functions.show_user_input(df, X, categorical_cols)      
+    input_df, user_input = functions.show_user_input(df, columns, categorical_cols)      
     prediction, prediction_label = functions.make_prediction(model, label_encoder, input_df)  
     functions.show_prediction(prediction_label)
 
