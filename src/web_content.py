b1af43b63f0a098cdac49435fa75c4b135023460
Sandeep Dixit <sdixit@ohioedge.com>
2024-07-20 11:56:39 -0400

    formatting

--- src/web_content.py
+++ src/web_content.py
@@ -7,20 +7,20 @@ def write_content(resources_folder):
     st.title('Karma Coordinates Calculator App')
 
     # web content
-    st.image(f'{resources_folder}/kapil-muni-image.png', caption='Kapil muni 5000 BC')
+    st.image(f'{resources_folder}/kapil-muni-image.png', caption='Kapil Muni 5561 BCE')
 
     pdf = f'<a href="{resources_folder}/samkhya-karika.pdf">pdf</a>'
 
     about_karma_coordinates = ''' 
-According to *Sankhya*-yoga, a doctrine of Kapil *Muni* (Sage) dating back to 5561 BCE (https://en.wikipedia.org/wiki/Samkhya), *Prakriti* (the universe) exists for providing experiences to *Purush* (an experiencer). *Purush* upon realization that “I exist” achieves *Moksha* (liberation). *Prakriti* is constructed of three *Gunas* (root physical properties) - *Satva* (light), *Rajas* (energy) and *Tamas* (mass)  and *Purush* is an unobservable entity outside of *Prakriti* that is not constructed of these three *Gunas*.
+According to *Sankhya*, a doctrine acredited to Kapil *Muni* (Sage) dating back to 5561 BCE, *Prakriti* (the universe) exists for providing experiences to *Purush* (an experiencer). *Purush* upon realization that “I exist” achieves *Moksha* (liberation). *Prakriti* is constructed of three *Guna* (root physical properties) - *Satva* (light), *Raja* (energy) and *Tama* (mass)  and *Purush* is an unobservable entity outside of *Prakriti* that is not constructed of these three *Gunas*. (Aniruddha. et al., 1915; Wikipedia contributors, 2024).
 
-Every *Jiva* (life-form) in *Prakriti* is engaged in providing experiences to a *Purush* that trigger *Bhava*. A life-form comes into an existence due to *Sukshm* (a microscopic particle in the nature). It is *Sukshm* that gets tinged with *Bhava* (acquired tendencies), from a life-form to life-form! Once all *Bhava* are overcome/consumed, *Purush* realizes that "I exist" and *Moksha* is achieved.
+Every *Jiva* (life-form) in *Prakriti* is engaged in providing experiences to a *Purush* that incite *Bhava*. *Jiva* comes into an existence due to *Sukshm* (a microscopic particle). It is *Sukshm* that gets tinged with *Bhava* (acquired tendencies), carrying *Bhava* from existence to existence! Once all *Bhava* are overcome/consumed, *Purush* realizes/knows that "I exist" and *Moksha* is achieved.
 
-Once  all *Purush* are awakened, *Prakriti*’s work is complete and it collapses into a singularity and the next cycle big-bang - big-crunch cycle is started. As per the open model with omega of 6, universe is 6 billion years old and it will end when it is about 13 billion years old - that is in another 7 billion years (https://en.wikipedia.org/wiki/Ultimate_fate_of_the_universe). A human life is the only known form of *Sukshm* that is capable of achieving *Moksha* for its *Purush*. 
+Once  all *Purush* awake, *Prakriti*’s work is complete and it collapses into a singularity and the next cycle of big-bang - big-crunch begins. As per the open model with omega of 6, universe is 6 billion years old and it will end when it is about 13 billion years old - that is in another 7 billion years. (Wikipedia contributors. (2024, June 27); Ultimate fate of the universe). A human life is the only known form of *Sukshm* that is capable of achieving *Moksha* for its *Purush*. 
 
 *Sankhya*-yoga quantifies *Bhava* (tendencies), overcoming which takes a *Sukshm* closer to achieving *Moksha*. Karma Coordinates is a fun app developed to calculate your current position in this karmic journey of many lives - from the beginning to *Moksha*, by approximating your current *Bhava*. 
 
-Very important to know that one can exponentially speed-up karmic journey by certain lifestyle changes. *Moksha* is achieved during ***one*** final life-form!
+You can exponentially speed-up karmic journey by small lifestyle changes.
 
 Karma Coordinates outcome is also explained in terms of three *Gunas*:  
     - ***Satva*** is the *Prakash* (light) property in the *Prakriti*. The neural network in our brain - our intellect - has the highest *Satva*.  
@@ -36,6 +36,10 @@ def write_sankhya_references():
     subheader = "References"
     references = '''
     Aniruddha. Vritti; Vijñanabhiksu, fl. 1550. Bhasya; Mahadeva Vedantin. Vritti-sara; Translated by: Nandlal Sinha (1915). ***The samkhya philosophy***. Available at: https://archive.org/details/thesamkhyaphilos00sinhuoft.
+
+    Wikipedia contributors. (2024, July 19). Samkhya. In Wikipedia, The Free Encyclopedia. Retrieved 15:34, July 20, 2024, from https://en.wikipedia.org/w/index.php?title=Samkhya&oldid=1235515297
+
+    Wikipedia contributors. (2024, June 27). Ultimate fate of the universe. In Wikipedia, The Free Encyclopedia. Retrieved 15:36, July 20, 2024, from https://en.wikipedia.org/w/index.php?title=Ultimate_fate_of_the_universe&oldid=1231304126    
 '''
     st.subheader(subheader)
     st.markdown(references)
