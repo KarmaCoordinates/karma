0941965db86dd424700c613521d5376f0d2f5cc8
Sandeep Dixit <sdixit@ohioedge.com>
2024-07-25 18:19:57 -0400

    text update

--- src/whitepaper_to_html.py
+++ src/whitepaper_to_html.py
@@ -1,6 +1,24 @@
 import markdown
+import file_functions as ff
+import os
 
 def md_to_html(static_files_folder, md_filename, html_filename):
-    markdown.markdownFromFile(input=f'{static_files_folder}/{md_filename}', output=f'{static_files_folder}/{html_filename}', encoding="utf-8")
+    bucket_name = 'karmacoordinates'
+    object_key = html_filename
+
+    md_filename_path = f'{static_files_folder}/{md_filename}'
+    html_filename_path = f'{static_files_folder}/{html_filename}'
+
+    markdown.markdownFromFile(input=md_filename_path, output=html_filename_path, encoding="utf-8")
+
+    with open(html_filename_path, 'r') as original:
+        data = original.read()
+    with open(html_filename_path, 'w') as modified:
+        modified.write('<meta http-equiv="Content-Type" content="text/html;charset=utf-8">\n' + data)    
+
+    ff.save_file_to_s3(html_filename_path, bucket_name, object_key)
+
+    os.remove(html_filename_path)
+
 
 md_to_html('.static', 'calculating_karma_coordinates.md', 'calculating_karma_coordinates.html')
