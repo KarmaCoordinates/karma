45c04fd7d12f138aa6b602695c2dd5dbe71df7c5
Sandeep Dixit <sdixit@ohioedge.com>
2024-07-27 21:18:17 -0400

    text update

--- src/web_content.py
+++ src/web_content.py
@@ -44,9 +44,11 @@ def sankhya_references():
 
     Wikipedia contributors. (2024, July 19). *Samkhya*. In Wikipedia, The Free Encyclopedia. Retrieved 15:34, July 20, 2024, from https://en.wikipedia.org/w/index.php?title=Samkhya&oldid=1235515297
 
-    Wikipedia contributors. (2024, June 27). *Ultimate fate of the universe*. In Wikipedia, The Free Encyclopedia. Retrieved 15:36, July 20, 2024, from https://en.wikipedia.org/w/index.php?title=Ultimate_fate_of_the_universe&oldid=1231304126    
+    Wikipedia contributors. (2024, July 22). *Age of the universe*. In Wikipedia, The Free Encyclopedia. Retrieved 23:46, July 27, 2024, from https://en.wikipedia.org/w/index.php?title=Age_of_the_universe&oldid=1236032234
 
-     Sandeep Dixit, 2024. *Calculating Karma Coordinates*, https://karmacoordinates.s3.us-east-2.amazonaws.com/calculating_karma_coordinates.html
+    Wikipedia contributors. (2024, June 27). *Ultimate fate of the universe*. In Wikipedia, The Free Encyclopedia. Retrieved 15:36, July 20, 2024, from https://en.wikipedia.org/w/index.php?title=Ultimate_fate_of_the_universe&oldid=1231304126
+
+    Sandeep Dixit, 2024. *Calculating Karma Coordinates*, https://karmacoordinates.s3.us-east-2.amazonaws.com/calculating_karma_coordinates.html
 '''
     st.subheader(subheader)
     st.markdown(references)
