d173f5e43698fb6af79b46cf4d973f868a88fb13
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-17 13:37:16 -0400

    formatting

--- src/openai_assistant_chat.py
+++ src/openai_assistant_chat.py
@@ -13,13 +13,12 @@ import numpy
 import input_functions as ifunc
 
 # Initialise session state to store conversation history locally to display on UI
-def init():
+def _init():
     if "chat_history" not in st.session_state:
         st.session_state.chat_history = []
 
     if "query_history" not in st.session_state:
         st.session_state.query_history = []
-
 
     if "query_queue" not in st.session_state:
         st.session_state.query_queue = []
@@ -27,42 +26,36 @@ def init():
         object_key = 'karma_coordinates_prompts.csv'
         ifunc.init_buttons(ff.cache_csv_from_s3(bucket_name, object_key).iloc[1:, 0].to_list())        
 
-    # Title
-    st.subheader("Your AI Assistant")
-
-
-def callback_button_on_click(key):
+def _callback_button_on_click(key):
     # print(f'key:{key}')
     st.session_state.query_queue.append(key)
     st.session_state.query_history.append(key)
     st.session_state.button_list.remove(key)
 
-
-
-def render_user_input(user_query_container):
+def _render_user_input(user_query_container):
     ai_default_question = 'How can I help you?'
     with user_query_container:
+
+        st.markdown('FAQs')
+        # show suggested options
+        ifunc.render_buttons(_callback_button_on_click)
+
         # draw user input box
-        user_query = st.chat_input(ai_default_question)
-
-        # draw pills
-        ifunc.render_buttons(callback_button_on_click)
-
+        user_query = st.chat_input(ai_default_question)        
         if (user_query is not None) and not (user_query in st.session_state.query_history):
             st.session_state.query_queue.append(user_query)
             st.session_state.query_history.append(user_query)
 
 
 # Display messages in chat history
-def render_chat_history():
+def _render_chat_history():
     for message in st.session_state.chat_history:
         with st.chat_message(message["role"]):
             st.markdown(message["content"])
     
 
-
 # Function to check if there is an active run
-def check_active_run(client, thread_id):
+def _check_active_run(client, thread_id):
     try:
         # Fetch runs for the thread and check their status
         active_runs = client.beta.threads.runs.list(thread_id=thread_id)
@@ -75,8 +68,7 @@ def check_active_run(client, thread_id):
         return False
 
 
-
-def process_prompt(client, assistant, user_query):    
+def _process_prompt(client, assistant, user_query):    
     # Display the user's query
     with st.chat_message("user"):
         st.markdown(user_query)
@@ -145,7 +137,7 @@ def process_queue(client, assistant, process_prompt_container):
 
     while st.session_state.query_queue and len(st.session_state.query_queue) > 0:
         # Wait until there is no active run
-        while check_active_run(client, st.session_state.thread_id):
+        while _check_active_run(client, st.session_state.thread_id):
             time.sleep(1)  # Wait for 10 seconds before checking again      
 
         queued_user_query = st.session_state.query_queue.pop(0)
@@ -153,17 +145,18 @@ def process_queue(client, assistant, process_prompt_container):
 
         # Streaming process
         with process_prompt_container:
-            process_prompt(client, assistant, queued_user_query)      
+            _process_prompt(client, assistant, queued_user_query)      
 
 
 def prompt():
     _configs = configs.config()
-    init()
+    _init()
+    st.subheader("Your AI Assistant")
     with st.container(border=True):
         user_query_container = st.container()
         process_prompt_container = st.container() # placeholder to keep current response above history
         st.divider()
-        render_chat_history()        
-        render_user_input(user_query_container=user_query_container)
+        _render_chat_history()        
+        _render_user_input(user_query_container=user_query_container)
         process_queue(client=_configs['openai_client'], assistant=_configs['openai_assistant'], process_prompt_container=process_prompt_container)
 
