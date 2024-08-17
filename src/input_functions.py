d9d87998509f354999f8fc6ab47e570999a924f9
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-17 13:20:27 -0400

    formatting

--- src/input_functions.py
+++ src/input_functions.py
@@ -18,23 +18,38 @@ def render_buttons(on_click_callback):
     st.markdown("""
             <style>
                 div[data-testid="column"] {
-                    width: fit-content !important;
                     flex: unset;
+                    width: fit-content !important;
+                    justify-content: start !important;
+                    margin: -0.3em !important;                
                 }
                 div[data-testid="column"] * {
                     width: fit-content !important;
-                    padding: -1px -1px !important;
-                    margin: -1px -1px !important;
-                    color: white !important;
-                    # background-color: green !important;
-                    # border: -20px !important;
-                    # font-size: 16px;
-                    height: auto !important;
-                    border-radius: 12px !important;
+                    padding: 0.015em;
+                    margin: -0.035em;
+                    border-radius: 16px;
+                }
+                div[data-testid="column"] button {
+                    height: 1.9em;
+                    padding: 0 0.3em 0 0.3em;
+                    align-items: center;
+                    justify-content: center;
+                }
+                div[data-testid="column"] button * {
+                    padding: 0;
+                    margin: -0.1em;
                 }
             </style>
             """, unsafe_allow_html=True)    
     
-    for text, col in list(map(list, zip(button_list, st.columns(len(button_list))))):
+    len_list = []
+    total_len_list = len(button_list)
+    for ele in button_list:
+        len_list.append(len(ele)/total_len_list)
+
+    # print(len_list)
+
+    for text, col in list(map(list, zip(button_list, st.columns(len_list)))):
+        # print(f'text:{text}, col{col}')
         with col:
             st.button(label=text, key=text, on_click=on_click_callback, args={text})
