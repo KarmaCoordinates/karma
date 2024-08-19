7dfc760443eba1a8bc62226afd5d45a2441b8644
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-18 21:59:13 -0400

    formatting

--- src/input_functions.py
+++ src/input_functions.py
@@ -15,40 +15,37 @@ def render_buttons(on_click_callback, button_list_name):
         return
 
     st.markdown("""
-            <style>
-                div[data-testid="column"] {
-                    flex: unset;
-                    width: fit-content !important;
-                    justify-content: start !important;
-                    margin: -0.3em !important;                
-                }
-                div[data-testid="column"] * {
-                    width: fit-content !important;
-                    padding: 0.015em;
-                    margin: -0.035em;
-                    border-radius: 16px;
-                }
-                div[data-testid="column"] button {
-                    height: 1.9em;
-                    padding: 0 0.3em 0 0.3em;
-                    align-items: center;
-                    justify-content: center;
-                }
-                div[data-testid="column"] button * {
-                    padding: 0;
-                    margin: -0.1em;
-                }
-            </style>
-            """, unsafe_allow_html=True)    
-    
-    len_list = []
-    total_len_list = len(button_list)
-    for ele in button_list:
-        len_list.append(len(ele)/total_len_list)
+        <style>
+            div[data-testid="column"] {
+                flex: 0 3 max-content !important;  
+                padding: 0;          
+                min-width: min-content;                   
+                justify-content: start !important;
+                # background-color: gray;
+                margin: -0.3em !important;        
+            }
+            div[data-testid="column"] * {
+                width: fit-content !important;
+                padding: 0.015em;
+                margin: -0.035em;
+                border-radius: 16px;
+                # background-color: red;     
+            }
+            div[data-testid="column"] button {
+                height: 1.9em;
+                padding: 0 0.3em 0 0.3em;
+                align-items: center;
+                justify-content: center;
+            }
+            div[data-testid="column"] button * {
+                padding: 0;
+                margin: -0.1em;
+            }
+                
+        </style>
+        """, unsafe_allow_html=True)    
 
-    # print(len_list)
-
-    for text, col in list(map(list, zip(button_list, st.columns(len_list)))):
-        # print(f'text:{text}, col{col}')
+    plh = plh = st.empty()
+    for text, col in list(map(list, zip(button_list, plh.columns(len(button_list))))):
         with col:
-            st.button(label=text, key=text, on_click=on_click_callback, args={text})
+            col.button(label=text, key=text, on_click=on_click_callback, args={text})
