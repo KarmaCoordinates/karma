e9285173a7e90332265470c74027303b305cd85b
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-18 22:15:33 -0400

    formatting

--- src/input_functions.py
+++ src/input_functions.py
@@ -18,11 +18,11 @@ def render_buttons(on_click_callback, button_list_name):
         <style>
             div[data-testid="column"] {
                 flex: 0 3 max-content !important;  
-                padding: 0;          
                 min-width: min-content;                   
                 justify-content: start !important;
-                # background-color: gray;
                 margin: -0.3em !important;        
+                padding: 0;          
+                # background-color: gray;
             }
             div[data-testid="column"] * {
                 width: fit-content !important;
@@ -33,13 +33,13 @@ def render_buttons(on_click_callback, button_list_name):
             }
             div[data-testid="column"] button {
                 height: 1.9em;
-                padding: 0 0.3em 0 0.3em;
                 align-items: center;
                 justify-content: center;
+                padding: 0 0.3em 0 0.3em;
             }
             div[data-testid="column"] button * {
-                padding: 0;
                 margin: -0.1em;
+                padding: 0;
             }
                 
         </style>
