e0e9a4481e7152abf22025748514905a4461b653
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-23 02:53:06 -0400

    perf update

--- src/questionnaire_pratyay_sargah.py
+++ src/questionnaire_pratyay_sargah.py
@@ -3,21 +3,12 @@ import numpy as np
 import pandas as pd
 import s3_functions as s3f
 
-# categories_descriptions = {
-#     'Viparyayah (विपर्यय)' : '''This term refers to wrong perception or misunderstanding. It denotes a state of error or ignorance where one misapprehends reality, often due to an inversion of the true understanding. In a more detailed context within Samkhya, it can relate to the various forms of ignorance that distort one's understanding of the self and the world''',
-#     'Aśakti (अशक्ति)' : '''Ashakti translates to incapacity or inability. It signifies a state where one is unable to perform actions effectively due to various impairments, particularly regarding the sensory organs or cognitive functions. This incapacity can manifest in different forms, contributing to individuals' difficulties in discerning truths or fulfilling their potential''',
-#     'Tuṣṭi (तुष्टि)' : '''Tushti means contentment or satisfaction. It refers to a state of being pleased or content with one's circumstances or understanding. In Samkhya philosophy, it can signify a mental state that arises when the mind is at peace with reality, achieving a sense of well-being''',
-#     'Siddhi (सिद्धि)' : '''Siddhi denotes perfection or accomplishment. It implies the attainment of goals, mastery over certain skills, or realization of one's potential. In spiritual contexts, it can refer to the acquisition of supernatural powers or significant spiritual achievements. Siddhi is often discussed in relation to personal growth and the realization of one's true nature''',
-#     'Lifestyle' : '''The typical way of life -  interests, opinions, behaviours, and behavioural orientations - of an individual'''
-#  }
-
-
 @st.cache_data
 def _cache_questionnaire(bucket_name, features_data_dict_object_key, categories_data_dict_object_key):
     features_df = s3f.cache_csv_from_s3(bucket_name=bucket_name, object_key=features_data_dict_object_key)
+    features_df['options_dict'] = features_df.apply(lambda row: {**{row['Option_1'] : row['Value_1']}, **{row['Option_2'] : row['Value_2']}, **{row['Option_3'] : row['Value_3']}, **{row['Option_4'] : row['Value_4']}}, axis=1)
+    features_df['options_list'] = [list(item) for item in zip(features_df['Option_1'], features_df['Option_2'], features_df['Option_3'], features_df['Option_4']) ]
     categories_df = s3f.cache_csv_from_s3(bucket_name=bucket_name, object_key=categories_data_dict_object_key)
-    # df.columns = df.iloc[0] # first row is column names
-    # df = df[1:] 
     return features_df, categories_df
 
 
@@ -38,10 +29,9 @@ def _calc_scores():
         total_scores[category_tpl.category_name] = 0
         st.markdown(f'{category_tpl.category_name} Assessment - {category_tpl.category_description}')
         for feature_tpl in features_df.loc[features_df['Category'] == category_tpl.category_name].itertuples():
-            option_names = [x for x in [feature_tpl.Option_1, feature_tpl.Option_2, feature_tpl.Option_3, feature_tpl.Option_4] if str(x) != 'nan']
-            options_value = [x for x in [feature_tpl.Value_1, feature_tpl.Value_2, feature_tpl.Value_3, feature_tpl.Value_4] if str(x) != 'nan']
-            selected_option = st.radio(feature_tpl.Question, option_names, key=feature_tpl.Question, on_change=cache_user_input, args={feature_tpl.Question})
-            selected_option_score = options_value[option_names.index(selected_option)] 
+            options_list = pd.Series(feature_tpl.options_list).dropna().to_list()
+            selected_option = st.radio(feature_tpl.Question, options_list, key=feature_tpl.Question, on_change=cache_user_input, args={feature_tpl.Question})
+            selected_option_score = feature_tpl.options_dict.get(selected_option) 
             total_scores[category_tpl.category_name] += selected_option_score  
 
 def process_questions():
@@ -84,9 +74,4 @@ def get_score():
 
     return score_md
 
-
-
-# _init()
-# process_questions()
-# show_score()        
 
