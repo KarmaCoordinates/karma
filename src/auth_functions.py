2c94136f3d26935c71c63b035e8b2e28a24de2ad
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-25 13:07:32 -0400

    formatting

--- src/auth_functions.py
+++ src/auth_functions.py
@@ -29,14 +29,14 @@ def send_email(recipient, token):
             server.login(smtp_username, smtp_password)
             server.sendmail(sender_email, recipient, msg)
 
-        sending_email_bar.success("Token emailed!")
+        sending_email_bar.success("Emailed!")
 
     except Exception as e:
-        sending_email_bar.error(f"Error emailing: {e}")
+        sending_email_bar.error(f"Error: {e}")
 
 # Streamlit app
 def do_2fa():
-    st.markdown("Confirm identity:")
+    st.markdown("Confirm your identity:")
 
     if 'auth' in st.session_state and st.session_state.auth:
         reset_data()
@@ -45,13 +45,29 @@ def do_2fa():
         show_2fa()
 
 def show_2fa():
-    col1, col2, col3, col4 = st.columns([.45, .15, .25, .15])
+    # ele_list = [{'key':'enter_email', 'label':'Enter your email:', 'type':'text_input', 'callback': on_click_callback, 'args': {'ele1'}, 'max-width': '280px'}, 
+    #             {'key':'ti_2', 'label':'Send Token', 'type':'button', 'callback': on_click_callback, 'args': {'ele2'}}, 
+    #             {'key':'ti_3', 'label':'ele3', 'type':'text_input', 'callback': on_click_callback, 'args': {'ele3'}, 'max-width': '70px'}, 
+    #             {'key':'ti_4', 'label':'Verify Token', 'type':'button', 'callback': on_click_callback, 'args': {'ele4'}}]
+    # render_elements(ele_list)
+
+    col1, col2, col3, col4 = st.columns([4,1,2,1])
     with col1:
         # User input for email
+        col1.markdown(
+            """
+            <style>
+                [aria-label="Enter your email:"] {
+                        max-width: 150px !important;
+                }  
+            </style>
+            """,
+            unsafe_allow_html=True
+        )            
         email = col1.text_input("Enter your email:", key='enter_email')
     with col2:
         if 'enter_email' in st.session_state and st.session_state.enter_email:
-            send_token = col2.button("Send Token", key='send_token_button')
+            send_token = col2.button(label="Send Token", key='send_token_button', use_container_width=True)
 
         if 'send_token_button' in st.session_state and st.session_state.send_token_button:
             # Generate a 2FA token
@@ -62,11 +78,21 @@ def show_2fa():
 
     with col3:        
         if 'token' in st.session_state:
-            user_token = col3.text_input("Enter received token:", key='enter_token', max_chars=8)
+            col3.markdown(
+                """
+                <style>
+                    [aria-label="Enter token:"] {
+                            max-width: 70px !important;
+                    }  
+                </style>
+                """,
+                unsafe_allow_html=True
+            )            
+            user_token = col3.text_input("Enter token:", key='enter_token', max_chars=8)
 
     with col4:
         if 'enter_token' in st.session_state and st.session_state.enter_token:
-            verify_token = col4.button("Verify Token", key='verify_token_button')
+            verify_token = col4.button("Verify Token", key='verify_token_button', use_container_width=True)
             if verify_token:
                 if user_token == st.session_state.token:
                     st.session_state['auth']=True
--- src/streamlit_button_list.py
+++ src/streamlit_button_list.py
@@ -5,7 +5,18 @@ def render_buttons(button_list, on_click_callback, button_list_name = None, styl
         return
     
     if not style_css:
-        style_css = """
+        style_css = get_style_css()
+
+    st.markdown(style_css, unsafe_allow_html=True)    
+
+    plh = st.empty()
+    for text, col in list(map(list, zip(button_list, plh.columns(len(button_list))))):
+        with col:
+            col.button(label=text, key=text, on_click=on_click_callback, args={text})
+
+
+def get_style_css(ele_list=None):
+    style_css = """
         <style>
             div[data-testid="column"] {
                 flex: 0 3 max-content !important;  
@@ -32,16 +43,48 @@ def render_buttons(button_list, on_click_callback, button_list_name = None, styl
                 margin: -0.1em;
                 padding: 0;
             }
-                
-        </style>
-        """
+            div[data-testid="column"] input * {
+                margin: -0.1em;
+                padding: 0;
+            }
+            """
+    
 
-    st.markdown(style_css, unsafe_allow_html=True)    
+    if ele_list:
+        for ele in ele_list:
+            if 'max-width' in ele:
+                ele_style = f''' 
+                    [aria-label="{ele['label']}"] {{
+                            max-width: {ele['max-width']} !important;
+                            # padding: 10px;
+                            # border: 2px solid #4CAF50; /* Green border */
+                            # border-radius: 5px; /* Rounded corners */
+                            # font-size: 14px; /* Input text size */
+                    }}  
+                '''
+                style_css = style_css + ele_style
 
+    style_css = style_css + '</style>'
+        
+
+    return style_css
+
+
+# Example usage:
+# ele_list = [{'key':'ti_1', 'label':'ele1', 'type':'text_input', 'callback': on_click_callback, 'args': {'ele1'}, 'max-width': '280px'}, 
+#             {'key':'ti_2', 'label':'Send Token', 'type':'button', 'callback': on_click_callback, 'args': {'ele2'}}, 
+#             {'key':'ti_3', 'label':'ele3', 'type':'text_input', 'callback': on_click_callback, 'args': {'ele3'}, 'max-width': '70px'}, 
+#             {'key':'ti_4', 'label':'Verify Token', 'type':'button', 'callback': on_click_callback, 'args': {'ele4'}}]
+# render_elements(ele_list)
+def render_elements(ele_list):
+    st.markdown(get_style_css(ele_list), unsafe_allow_html=True)    
     plh = st.empty()
-    for text, col in list(map(list, zip(button_list, plh.columns(len(button_list))))):
+    for ele, col in list(map(list, zip(ele_list, plh.columns(len(ele_list))))):
         with col:
-            col.button(label=text, key=text, on_click=on_click_callback, args={text})
+            if ele['type'] == 'text_input':
+                col.text_input(label=ele['label'], key=ele['label'], on_change=ele['callback'], args=ele['args'])
+            if ele['type'] == 'button':
+                col.button(label=ele['label'], key=ele['label'], on_click=ele['callback'], args=ele['args'])
 
 def main():
     pass
