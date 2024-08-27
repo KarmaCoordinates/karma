ccd9b79535bba3f6a313d7700a04cfb9c34ec49c
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-27 18:59:41 -0400

    err msg

--- src/dynamodb_functions.py
+++ src/dynamodb_functions.py
@@ -5,18 +5,12 @@ temp_folder = '.tmp'
 logging.basicConfig(filename=f'{temp_folder}/kc-app.log', filemode='w', level=logging.INFO)
 
 def insert(user_activity_data):
-    # Attempt to create a session
-    try:
-        session = boto3.Session()  # This uses the default profile
-        dynamodb = session.resource('dynamodb')
-        table = dynamodb.Table('kc_user_activity')
-        response = table.put_item(
-            Item=user_activity_data
-        )
-    except Exception as e:
-        logging.error(f'Unable to insert:{e}.')
-        # print(f"Error: {e}")    
-
+    session = boto3.Session()  # This uses the default profile
+    dynamodb = session.resource('dynamodb')
+    table = dynamodb.Table('kc_user_activity')
+    response = table.put_item(
+        Item=user_activity_data
+    )
 
 def main():
     insert()
