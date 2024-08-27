6ab4ae3f2f366dfc3a651783b31c6f030dd11e68
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-27 16:01:13 -0400

    missing region fixed

--- src/dynamodb_functions.py
+++ src/dynamodb_functions.py
@@ -2,7 +2,7 @@ import boto3
 
 
 def insert(user_activity_data):
-    dynamodb = boto3.resource('dynamodb')
+    dynamodb = boto3.resource('dynamodb', region_name='us-east-2' )
     table = dynamodb.Table('kc_user_activity')
     response = table.put_item(
         Item=user_activity_data
