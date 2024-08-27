3c3457fc579f62a61422c4530681bc7dd870319d
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-26 20:56:32 -0400

    formatting

--- src/questionnaire_pratyay_sargah.py
+++ src/questionnaire_pratyay_sargah.py
@@ -4,6 +4,7 @@ import pandas as pd
 import s3_functions as s3f
 import score_functions as sf
 import configs
+import random
 
 @st.cache_data
 def _cache_questionnaire(bucket_name, features_data_dict_object_key, categories_data_dict_object_key):
@@ -17,8 +18,6 @@ def _cache_questionnaire(bucket_name, features_data_dict_object_key, categories_
     value_columns = features_df[['Value_1', 'Value_2', 'Value_3', 'Value_4']]
     minimum_score = value_columns.min(axis=1).sum()
     maximum_score = value_columns.max(axis=1).sum()
-    # total_min = min_values.sum()
-    # total_max = max_values.sum()
      
     return features_df, categories_df, {'minimum_score':minimum_score, 'maximum_score':maximum_score, 'number_of_questions':len(features_df)}
 
@@ -60,29 +59,6 @@ def _process_questions():
     input_df = pd.DataFrame(st.session_state.user_answers, index=[0])
     return input_df, st.session_state.user_answers, category_scores, sum(category_scores.values()), _get_score_analysis_query(category_scores), score_range
 
-# def show_score():
-#     # Display total scores and calculate total clarity of thinking index
-#     st.subheader("Total Scores Summary:")
-#     clarity_of_thinking_index = sum(total_scores.values())
-#     score_md = ''
-#     for category, score in total_scores.items():
-#         score_md = score_md + f'{category} Score: {score}'
-#         st.write(f"{category} Score: {score}")
-
-#     # Display clarity of thinking index
-#     st.write(f"Your Clarity of Thinking Index: {clarity_of_thinking_index}")
-#     score_md = score_md + f"Your Clarity of Thinking Index: {clarity_of_thinking_index}"
-
-#     return score_md
-
-    # Providing interpretation of the scores
-    # if clarity_of_thinking_index < 0:
-    #     st.write("You might want to work on your clarity of thinking.")
-    # elif clarity_of_thinking_index == 0:
-    #     st.write("You are at a neutral level. Consider focusing on personal growth.")
-    # else:
-    #     st.write("You have a good level of clarity of thinking and insight!")
-
 def _get_score_analysis_query(category_scores):
     clarity_of_thinking_index = sum(category_scores.values())
     score_md = ''
@@ -100,24 +76,13 @@ def assessment():
         input_df, user_answers, category_scores, total_score, score_ai_analysis_query, score_range = _process_questions()
         st.divider()
         plh_kc = st.empty()
-        # st.subheader('AI analysis')
         plh = st.container()
-        # clicked = st.button('Show and explain my score', on_click=openai_assistant_chat.prompt_specific, args=(qps.get_score(), plh))
-        # clicked = st.button('Show and explain my score')
-        # if clicked:
         percent_completed = len(st.session_state.user_answers) * 100 / score_range['number_of_questions']
         if percent_completed > st.session_state.minimum_required_completion_percent:
-            # st.session_state['ai_analysis_requested'] = True
             st.session_state['karma_coordinates'] = category_scores
             live_to_moksha = sf.calculate_karma_coordinates(category_scores, score_range)
             plh_kc.markdown(f':orange-background[$$\\large\\space Number\\space of \\space lives \\space to \\space Moksha:$$ $$\\huge {live_to_moksha} $$] $$\\small (Based\\space on\\space {round(percent_completed)}\\% \\space assessment.)$$')
-            # openai_assistant_chat.prompt_specific(score_ai_analysis_query, plh)                
-            # st.session_state.minimum_required_completion_percent = min(round(st.session_state.minimum_required_completion_percent * 1.2), 100)
         else:
-            # if st.session_state['ai_analysis_requested']: 
-                #ai_analysis was requested before, now increment the limit
-                # st.session_state.minimum_required_completion_percent = min(round(st.session_state.minimum_required_completion_percent * 1.2), 100)
-                # st.session_state['ai_analysis_requested'] = False
             st.warning(f'Atleast {round(st.session_state.minimum_required_completion_percent)}\\% of assessment needs to be completed to see Karma Coordinates.')
     return user_answers, score_ai_analysis_query, percent_completed
 
