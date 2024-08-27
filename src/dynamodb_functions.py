3cbeb98b423e7e5fc9ee1ad0256f1d8dae1ce550
Sandeep Dixit <sdixit@ohioedge.com>
2024-08-27 16:45:31 -0400

    dynamodb test

--- src/dynamodb_functions.py
+++ src/dynamodb_functions.py
@@ -2,10 +2,17 @@ import boto3
 
 
 def insert(user_activity_data):
-    dynamodb = boto3.resource('dynamodb', region_name='us-east-2' )
-    table = dynamodb.Table('kc_user_activity')
-    response = table.put_item(
-        Item=user_activity_data
+    # dynamodb = boto3.resource('dynamodb', region_name='us-east-2' )
+    dynamodb = boto3.client('dynamodb')
+
+    # table = dynamodb.Table('kc_user_activity')
+    response = dynamodb.put_item(
+        TableName='kc_user_activity',
+        Item={
+           'email': {'S': 'deleteme'},
+            'date': {'S': 'deleteme'},
+            'name': {'S': 'SomeName'}           
+            }
     )
 
 def main():
