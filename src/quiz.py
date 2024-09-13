2b45343702fe9fc0fbe9bef13a530845416bdc7c
Sandeep Dixit <sdixit@ohioedge.com>
2024-09-13 17:07:15 -0400

    quiz

--- src/quiz.py
+++ src/quiz.py
@@ -11,10 +11,10 @@ def take_quiz():
         plh_result = st.empty()
         with plh.container():        
             mem = '''What type of memory/knowledge is it?'''
-            answer1 = st.multiselect(label=f'''I can't see the emoji clearly: {grin_emoji}.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], max_selections=1, key="q_cannot_see_clearly")
-            answer2 = st.multiselect(label=f'''I see an unhappy face: {grin_emoji}.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], max_selections=1, key="q_unhappy_face")
-            answer3 = st.multiselect(label=f'''I see a happy face: {grin_emoji}.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], max_selections=1, key="q_happy_face")
-            answer4 = st.multiselect(label=f'''I am not interested in knowing.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], max_selections=1, key="q_donot_want_to_answer")
+            answer1 = st.selectbox(label=f'''I can't see the emoji clearly: {grin_emoji}.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], key="q_cannot_see_clearly", index=None)
+            answer2 = st.selectbox(label=f'''I see an unhappy face: {grin_emoji}.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], key="q_unhappy_face", index=None)
+            answer3 = st.selectbox(label=f'''I see a happy face: {grin_emoji}.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], key="q_happy_face", index=None)
+            answer4 = st.selectbox(label=f'''I am not interested in knowing.''', placeholder=mem, options=['Ignorance', 'Incapacity', 'Complecency', 'Siddhi'], key="q_donot_want_to_answer", index=None)
 
         # with plh.container():
         #     col1, col2 = st.columns([.5,.5])
@@ -45,7 +45,8 @@ def take_quiz():
         #     with col2:
         #         answer4 = st.selectbox('What type of memory is it?', ('Ignorance', 'Incapacity', 'Complecency', 'Siddhi'), key="q_donot_want_to_answer")
     
-        if len(st.session_state.q_cannot_see_clearly) > 0 and st.session_state.q_cannot_see_clearly[0] == 'Incapacity' and len(st.session_state.q_unhappy_face) == 1 and  st.session_state.q_unhappy_face[0] == 'Ignorance' and len(st.session_state.q_happy_face) == 1 and st.session_state.q_happy_face[0] == 'Siddhi' and len(st.session_state.q_donot_want_to_answer) == 1 and st.session_state.q_donot_want_to_answer[0] == 'Complecency':
+        # if len(st.session_state.q_cannot_see_clearly) > 0 and st.session_state.q_cannot_see_clearly[0] == 'Incapacity' and len(st.session_state.q_unhappy_face) == 1 and  st.session_state.q_unhappy_face[0] == 'Ignorance' and len(st.session_state.q_happy_face) == 1 and st.session_state.q_happy_face[0] == 'Siddhi' and len(st.session_state.q_donot_want_to_answer) == 1 and st.session_state.q_donot_want_to_answer[0] == 'Complecency':
+        if st.session_state.q_cannot_see_clearly == 'Incapacity' and  st.session_state.q_unhappy_face == 'Ignorance' and st.session_state.q_happy_face == 'Siddhi' and st.session_state.q_donot_want_to_answer == 'Complecency':                    
             st.session_state['quiz'] = True
             plh.success('Perfect!')
         else:
