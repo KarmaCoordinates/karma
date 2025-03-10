f042e0074edf5ead8c4de5e86d6af0ccb16e8b83
Sandeep Dixit <sdixit@ohioedge.com>
2025-03-10 19:44:03 -0400

    user answers update

--- src/fastapi_app.py
+++ src/fastapi_app.py
@@ -67,7 +67,9 @@ async def assessment_questionnaire(request: Request):
     if request.session.get('userId'):
         userAnswers = json.loads(request.session.get('userAnswers'))
         userAnswers[0].pop('_journal_entry', None)
+        userAnswers[0].pop('journal_entry', None)
         userAnswers[0].pop('feedback', None)
+        userAnswers[0].pop('email', None)
         # print(userAnswers)
         return json.dumps(userAnswers)
     else:
