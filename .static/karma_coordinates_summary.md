a19408b636051f367e15e3eb751aabae91c2487a
Sandeep Dixit <sdixit@ohioedge.com>
2025-05-03 10:31:04 -0400

    ui updates

--- .static/karma_coordinates_summary.md
+++ .static/karma_coordinates_summary.md
@@ -1,3 +1,43 @@
-<em>Sankhya</em>, a doctrine by Kapil <em>Muni</em> (Sage) dating back to 5561 BCE, articulates that the purpose of <em>Prakriti</em> (the universe) is to provide experiences to every <em>Jiv</em> (a life-form taken by <em>Sukshm</em> - a microsopic particle). Every experience results in a modification of <em>Buddhi</em> (intellect). A modification (what we call a memory) can be of <em>Viparyay</em> (Ignorance), <em>Ashakti</em> (Incapacity), <em>Tushti</em> (Complacency) or <em>Siddhi</em> (Real and true knowledge) type. With the achieving of pure <em>Siddhi</em> state the purpose of <em>Prakriti</em> is fulfilled - resulting in <em>Moksh</em> (end of <em>Sukshm's</em> journey of many lives/experiences) (Aniruddha. et al., 1915, p. 48-49, Shloka 64-66).
- 
-Sankhya.app Karma Coordinates calculator is a fun way to assess your current position in this karmic journey of many lives - from the beginning to <em>Moksh</em> - by approximating your current state of <em>Buddhi</em> (Sandeep Dixit, 2024. <em>Calculating Karma Coordinates</em>). It is important to know that even a small lifestyle change can put you on a fast-track - speeding your journey by thousands of years!
+<h3>What is Karma Coordinates?</h3>
+  <p>
+    {{logo}}
+    Karma Coordinates is an app for enhancing <strong>clarity of thinking</strong> through AI-based reflective journaling,
+    assessment, and recommended activities. The Karma Coordinates—the "Lives to Moksha" metric—serves as an
+    index to track progress toward greater clarity of thought. By focusing on clarity and using Karma Coordinates as a
+    guide, individuals can improve decision-making, foster personal growth, and achieve success in life!
+  </p>
+  <p>
+    Karma Coordinates is an app for enhancing <strong>clarity of thinking</strong> through AI-based reflective journaling,
+    assessment, and recommended activities. The Karma Coordinates—the "Lives to Moksha" metric—serves as an
+    index to track progress toward greater clarity of thought. By focusing on clarity and using Karma Coordinates as a
+    guide, individuals can improve decision-making, foster personal growth, and achieve success in life!
+  </p>
+
+<h3>How does it work?</h3>
+  <p> Karma Coordinates interprets your journal entries, uses that interpretation to answer relevant questions from the
+    Clarity of Thinking assessment questionnaire, and calculates your score. Initially, your score is based on minimal
+    completion of the questionnaire. As you continue to make new journal entries, the completion percentage goes up.
+  </p>
+
+  <h3>How is the assessment questionnaire prepared?</h3>
+  <p>
+    {{kapil_muni}}
+    The assessment questionnaire is based on <em>Sankhya</em> — a doctrine by Sage Kapil <em>Muni</em>, dating back to 5561 BCE—which
+    articulates that the purpose of the universe is to provide experiences to every life-form. Each experience results
+    in a physical modification of the brain or intellect (<em>Buddhi</em>), commonly referred to as memory. These modifications
+    are classified into four categories: Ignorance (<em>Viparyay</em>), Incapacity (<em>Ashakti</em>), Complacency (<em>Tushti</em>), or True Knowledge (<em>Siddhi</em>). 
+    Sankhya futher defines 5, 28, 9, and 8 subcategories for these categories respectively. The clarity of thinking questionnaire 
+    contains 50 questions to determine the current state of these 50 subcategories. In addition Lifestyle is added as a bonus category 
+    to take into account the modern ways of life. 
+    Attaining the state of <em>Siddhi</em> allows one to experience the ultimate truth. At that point, the purpose of the
+    universe is fulfilled—resulting in <em>Moksha</em>, the end of the life-form’s journey through many lives and experiences
+    (Aniruddha et al., 1915 <a href="https://archive.org/details/thesamkhyaphilos00sinhuoft/page/48/mode/2up">p. 48-49,
+      Shloka 64-66</a>).
+  </p>
+  <p>
+    Karma Coordinates is a fun way to assess your current position in the karmic journey across many
+    lives — from the beginning to <em>Moksha</em> — by approximating your current state of Buddhi (Sandeep Dixit, 2024. <a
+      href="https://karmacoordinates-public.s3.us-east-2.amazonaws.com/calculating_karma_coordinates.html"><em>Calculating
+      Karma Coordinates</em></a>). Even a small lifestyle change can put you on the fast track,
+    accelerating your journey by thousands of years!
+  </p>
--- src/streamlit_content.py
+++ src/streamlit_content.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import base64
+from jinja2 import Template
         
 def overview(static_files_folder):
     # logo = f'{static_files_folder}/kapil-muni-image.png'
-    logo = f'{static_files_folder}/sankhya-logo-transparent350x350.png'
-    intro = '''The app enhances clarity of thought through <span style="color:gold">reflective journaling, AI analytics and recommended activities, improving individuals' chances of success.</span> Karma Coordinates, specifically lives to Moksha, serves as an index to track progress towards mental clarity. By focusing on clarity and using Karma coordinates as a metric, individuals can improve decision-making, foster personal growth, and achieve success!'''
+    # logo = f'{static_files_folder}/sankhya-logo-transparent350x350.png'
+    # intro = '''The app enhances clarity of thought through <span style="color:gold">reflective journaling, AI analytics and recommended activities, improving individuals' chances of success.</span> Karma Coordinates, specifically lives to Moksha, serves as an index to track progress towards mental clarity. By focusing on clarity and using Karma coordinates as a metric, individuals can improve decision-making, foster personal growth, and achieve success!'''
     style = '''
         <style>
         .container {
@@ -35,58 +36,10 @@ def overview(static_files_folder):
     # )
 
     st.markdown(
-        f"""
-        {style}
-        <div class="container">
-            <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(logo, "rb").read()).decode()}" alt="Kapil Muni 5561 BCE">
-            <p class="logo-text">{intro}</p>
-        </div>
-        """,
+        f"""{style}""",
         unsafe_allow_html=True
-    )    
+    )        
 
-def auth_overview(static_files_folder):
-    logo = f'{static_files_folder}/kapil-muni-image.png'
-    intro = '''By focusing on clarity and using Karma coordinates as a metric, individuals can improve decision-making, foster personal growth, and increase success. <span style="color:gold">Daily journaling is an important technique of developing clarity.</span> Karma Coordinates AI interprets your daily journal entries and updates your score accordingly, creating a continuous picture of your journey to clarity!'''
-    style = '''
-        <style>
-        .container {
-            # display: flex;
-        }
-        .logo-text {
-            # font-weight:700 !important;
-            # font-size:50px !important;
-            # color: #f9a01b !important;
-            # padding-top: 75px !important;
-            # padding-left: 15px !important;
-        }
-        .logo-img {
-            object-fit: cover;
-            float: left;
-            width: auto;
-            height: 150px;
-            padding-right: 15px !important;
-        }
-        </style>
-        '''
-
-    # title
-    st.title('Karma Coordinates Calculator')
-    # st.markdown(
-    #     style,
-    #     unsafe_allow_html=True
-    # )
-
-    st.markdown(
-        f"""
-        {style}
-        <div class="container">
-            <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(logo, "rb").read()).decode()}" alt="Kapil Muni 5561 BCE">
-            <p class="logo-text">{intro}</p>
-        </div>
-        """,
-        unsafe_allow_html=True
-    )    
 
 
 def background(static_files_folder):
@@ -111,18 +64,25 @@ def background(static_files_folder):
         }
         </style>
         '''
-    st.subheader('Background')
-    logo = f'{static_files_folder}/kapil-muni-image.png'
-    pdf = f'<a href="{static_files_folder}/samkhya-karika.pdf">pdf</a>'
-    pdf_content = open(f'{static_files_folder}/karma_coordinates_summary.md', 'r').read()
-    about_karma_coordinates = f'''{pdf_content}'''
+    # st.subheader('How does it work?')
+    logo = f'{static_files_folder}/logo.png'
+    kapil_muni = f'{static_files_folder}/kapil-muni-image.png'
+    # pdf = f'<a href="{static_files_folder}/samkhya-karika.pdf">pdf</a>'
+    about_karma_coordinates = open(f'{static_files_folder}/karma_coordinates_summary.md', 'r').read()
     # st.markdown(about_karma_coordinates)
+
+    logo_str = f'<img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(logo, "rb").read()).decode()}"  alt="Karma Coordinates">'
+    
+    kapil_muni_str =  f'<img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(kapil_muni, "rb").read()).decode()}" alt="Kapil Muni 5561 BCE">'
+    
+    template = Template(about_karma_coordinates)
+    md = template.render(logo = logo_str, kapil_muni = kapil_muni_str )
 
     st.markdown(
         f'''
+        {md}
         <div class="container">
-            <img class="logo-img" src="data:image/png;base64,{base64.b64encode(open(logo, "rb").read()).decode()}" alt="Kapil Muni 5561 BCE">
-            <p class="logo-text">{about_karma_coordinates}</p>
+            <p class="logo-text"></p>
         </div>
         ''',
         unsafe_allow_html=True
